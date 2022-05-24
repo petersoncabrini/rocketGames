9dbfa8eb2503d7d79510a12d4e7d1fb3a7133ee1
Peterson <petersoncabrini@hotmail.com>
2022-05-23 21:40:35 -0300

    adicionadas validacoes no formulario de login

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,7 +1,18 @@
-const LoginCompraController = {
-    login: function(req, res){
+const { validationResult } = require('express-validator')
+
+const LoginController = {
+    login: function (req, res) {
         res.render('login');
+    },
+    criarConta: (req, res) => {
+        let errors = validationResult(req)
+        if (errors.isEmpty()) {
+            res.redirect('/home')
+        } else {
+            return res.render('login', { errors: errors.mapped(), old: req.body })
+        }
+        console.log(req.body)
     }
 }
 
-module.exports = LoginCompraController;
+module.exports = LoginController;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const {check} = require ('express-validator')
+
 const HomeController = require ('../controllers/HomeController')
 const ProductController = require ('../controllers/ProductController')
 const FinalizarCompraController = require ('../controllers/FinalizarCompraController')
@@ -8,6 +10,19 @@ const LoginController = require ('../controllers/LoginController')
 const Painelontroller = require ('../controllers/PainelController')
 const CarrinhoController = require ('../controllers/CarrinhoController')
 const ProductListController = require ('../controllers/ProductListController')
+
+let validadeLogin = [
+    check('name')
+    .notEmpty().withMessage('O campo deve ser preenchido').bail()
+    .isLength({min:5}).withMessage('O nome deve ter pelo menos 5 caracteres'),
+    check('email')
+    .notEmpty().withMessage('O campo deve ser preenchido').bail()
+    .isEmail().withMessage('Digite um email valido'),
+    check('password')
+    .notEmpty().withMessage('O campo deve ser preenchido').bail()
+    .isLength({min:6}).withMessage('A senha deve ter pelo menos 6 caracteres'),
+
+]
 
 
 router.get('/home', HomeController.home) 
@@ -19,6 +34,7 @@ router.get('/finalizarcompra', FinalizarCompraController.finalizarCompra)
 router.get('/sucesso', SucessoController.sucesso) 
 
 router.get('/login', LoginController.login) 
+router.post('/login/criar', validadeLogin, LoginController.criarConta)
 
 router.get('/productlist', ProductListController.productList) 
 

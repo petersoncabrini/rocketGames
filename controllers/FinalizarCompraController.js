dc754948a498ade142fd5925f76a2bcca7249b90
Peterson <petersoncabrini@hotmail.com>
2022-05-24 12:15:38 -0300

    adicionada validacoes e exibicao de erros na view finalizarCompra

--- controllers/FinalizarCompraController.js
+++ controllers/FinalizarCompraController.js
@@ -1,7 +1,17 @@
+const { validationResult } = require('express-validator')
 const FinalizarCompraController = {
-    finalizarCompra: function(req, res){
+    finalizarCompra: function (req, res) {
         res.render('finalizarCompra');
-    }
+    },
+    comprar: (req, res) => {
+        let errors = validationResult(req)
+        if (errors.isEmpty()) {
+            res.redirect('/home')
+        } else {
+            res.render('finalizarCompra', { errors: errors.mapped(), old: req.body })
+        }
+        console.log(req.body)
+    },
 }
 
 module.exports = FinalizarCompraController;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const {check} = require ('express-validator')
 
+// Controllers
 const HomeController = require ('../controllers/HomeController')
 const ProductController = require ('../controllers/ProductController')
 const FinalizarCompraController = require ('../controllers/FinalizarCompraController')
@@ -9,8 +10,10 @@ const SucessoController = require ('../controllers/SucessoController')
 const LoginController = require ('../controllers/LoginController')
 const PainelController = require ('../controllers/PainelController')
 const CarrinhoController = require ('../controllers/CarrinhoController')
-const ProductListController = require ('../controllers/ProductListController')
+const ProductListController = require ('../controllers/ProductListController');
 
+
+// Express-validator
 let validarCriarConta = [
     check('name')
     .notEmpty().withMessage('O campo nome deve ser preenchido').bail()
@@ -32,43 +35,77 @@ let validarEntrarConta = [
     .isLength({min:6}).withMessage('A senha deve ter pelo menos 6 caracteres')
 ]
 
-
 let validarEditarPainel =[
     check('Nome')
     .notEmpty().withMessage('O campo Nome deve ser preenchido').bail()
     .isLength({min:5}).withMessage('O Nome deve ter pelo menos 3 caracteres'),
     check('Sobrenome')
-    .notEmpty().withMessage('O campo Sobrenome deve ser preenchido').bail(),
+    .notEmpty().withMessage('O campo Sobrenome deve ser preenchido'),
     check('Usuario')
-    .notEmpty().withMessage('O campo Usuario deve ser preenchido').bail(),
+    .notEmpty().withMessage('O campo Usuario deve ser preenchido'),
     check('Email')
     .notEmpty().withMessage('O campo Email deve ser preenchido').bail()
     .isEmail().withMessage('Digite um email valido'),
     check('CPF')
     .notEmpty().withMessage('O campo CPF deve ser preenchido').bail()
-    .isInt().withMessage('O campo CPF aceita somente numeros').bail()
+    .isInt().withMessage('O campo CPF aceita somente numeros')
     .isLength({min:11, max:11}).withMessage('CPF Invalido'),
     check('Endereco')
     .notEmpty().withMessage('O campo Endereco deve ser preenchido').bail(),
     check('Numero')
     .notEmpty().withMessage('O campo Numero deve ser preenchido').bail()
-    .isInt().withMessage('O campo Numero aceita somente numeros').bail(),
+    .isInt().withMessage('O campo Numero aceita somente numeros'),
     check('Bairro')
-    .notEmpty().withMessage('O campo Bairro deve ser preenchido').bail(),
+    .notEmpty().withMessage('O campo Bairro deve ser preenchido'),
     check('Cidade')
-    .notEmpty().withMessage('O campo Cidade deve ser preenchido').bail(),
+    .notEmpty().withMessage('O campo Cidade deve ser preenchido'),
     check('Estado')
-    .notEmpty().withMessage('O campo Estado deve ser preenchido').bail(),
+    .notEmpty().withMessage('O campo Estado deve ser preenchido'),
     check('CEP')
     .notEmpty().withMessage('O campo CEP deve ser preenchido').bail()
-    .isLength({min:8, max:8}).withMessage('CEP Invalido')
+    .isPostalCode('BR').withMessage('CEP Invalido')
 ]
+
+let validarCompra = [
+    check('Endereco')
+    .notEmpty().withMessage('O campo Endereco deve ser preenchido').bail(),
+    check('Numero')
+    .notEmpty().withMessage('O campo Numero deve ser preenchido').bail()
+    .isInt().withMessage('O campo Numero aceita somente numeros'),
+    check('Bairro')
+    .notEmpty().withMessage('O campo Bairro deve ser preenchido'),
+    check('Cidade')
+    .notEmpty().withMessage('O campo Cidade deve ser preenchido'),
+    check('Estado')
+    .notEmpty().withMessage('O campo Estado deve ser preenchido'),
+    check('CEP')
+    .notEmpty().withMessage('O campo CEP deve ser preenchido').bail()
+    .isPostalCode('BR').withMessage('CEP Invalido'),
+    check('NumeroCartao')
+    .notEmpty().withMessage('O campo Numero do Cartao deve ser preenchido').bail()
+    .isCreditCard().withMessage('Digite um numero de cartao valido!'),
+    check('Validade')
+    .notEmpty().withMessage('O campo Validade deve ser preenchido').bail()
+    .isDate().withMessage('Digite uma data valida'),
+    check('NomeCartao')
+    .notEmpty().withMessage('O campo Nome no Cartao deve ser preenchido').bail()
+    .isLength({min:8}).withMessage('O nome e muito curto!'),
+    check('CodigoSeguranca')
+    .notEmpty().withMessage('O campo Codigo de Seguranca deve ser preenchido').bail()
+    .isInt().withMessage('O campo Codigo de Seguranca aceita somente numeros')
+    .isLength({min:3, max:3}).withMessage('Codigo de Seguranca Invalido'),
+    check('QuantidadeParcelas')
+    .isInt().withMessage('Escolha a quantidade de parcelas').bail()
+]
+
+// Rotas
 
 router.get('/home', HomeController.home) 
 
 router.get('/product', ProductController.product) 
 
 router.get('/finalizarcompra', FinalizarCompraController.finalizarCompra) 
+router.post ('/finalizarCompra/comprar', validarCompra, FinalizarCompraController.comprar)
 
 router.get('/sucesso', SucessoController.sucesso) 
 

441e2d377e054fb6ce1a4fa56ab81f31ef9cca00
Peterson <petersoncabrini@hotmail.com>
2022-05-24 19:21:02 -0300

    correcao ortografica na mensagem exibida na view

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ let validarCriarConta = [
     check('name')
     .notEmpty().withMessage('O campo nome deve ser preenchido').bail()
     .isLength({min:5}).withMessage('O nome deve ter pelo menos 3 caracteres')
-    .isAlpha().withMessage('Deve ser gigitado somente letras'),
+    .isAlpha().withMessage('Deve ser digitado somente letras'),
     check('email')
     .notEmpty().withMessage('O campo email deve ser preenchido').bail()
     .isEmail().withMessage('Digite um email valido'),
@@ -40,13 +40,13 @@ let validarEditarPainel =[
     check('Nome')
     .notEmpty().withMessage('O campo Nome deve ser preenchido').bail()
     .isLength({min:5}).withMessage('O Nome deve ter pelo menos 3 caracteres')
-    .isAlpha().withMessage('Deve ser gigitado somente letras'),
+    .isAlpha().withMessage('Deve ser digitado somente letras'),
     check('Sobrenome')
     .notEmpty().withMessage('O campo Sobrenome deve ser preenchido')
-    .isAlpha().withMessage('Deve ser gigitado somente letras'),
+    .isAlpha().withMessage('Deve ser digitado somente letras'),
     check('Usuario')
     .notEmpty().withMessage('O campo Usuario deve ser preenchido')
-    .isAlpha().withMessage('Deve ser gigitado somente letras'),
+    .isAlpha().withMessage('Deve ser digitado somente letras'),
     check('Email')
     .notEmpty().withMessage('O campo Email deve ser preenchido').bail()
     .isEmail().withMessage('Digite um email valido'),
@@ -56,19 +56,19 @@ let validarEditarPainel =[
     .isLength({min:11, max:11}).withMessage('CPF Invalido'),
     check('Endereco')
     .notEmpty().withMessage('O campo Endereco deve ser preenchido').bail()
-    .isAlpha().withMessage('Deve ser gigitado somente letras'),
+    .isAlpha().withMessage('Deve ser digitado somente letras'),
     check('Numero')
     .notEmpty().withMessage('O campo Numero deve ser preenchido').bail()
     .isInt().withMessage('O campo Numero aceita somente numeros'),
     check('Bairro')
     .notEmpty().withMessage('O campo Bairro deve ser preenchido')
-    .isAlpha().withMessage('Deve ser gigitado somente letras'),
+    .isAlpha().withMessage('Deve ser digitado somente letras'),
     check('Cidade')
     .notEmpty().withMessage('O campo Cidade deve ser preenchido')
-    .isAlpha().withMessage('Deve ser gigitado somente letras'),
+    .isAlpha().withMessage('Deve ser digitado somente letras'),
     check('Estado')
     .notEmpty().withMessage('O campo Estado deve ser preenchido')
-    .isAlpha().withMessage('Deve ser gigitado somente letras'),
+    .isAlpha().withMessage('Deve ser digitado somente letras'),
     check('CEP')
     .notEmpty().withMessage('O campo CEP deve ser preenchido').bail()
     .isPostalCode('BR').withMessage('CEP Invalido')
@@ -77,16 +77,16 @@ let validarEditarPainel =[
 let validarCompra = [
     check('Endereco')
     .notEmpty().withMessage('O campo Endereco deve ser preenchido').bail()
-    .isAlpha().withMessage('Deve ser gigitado somente letras'),
+    .isAlpha().withMessage('Deve ser digitado somente letras'),
     check('Numero')
     .notEmpty().withMessage('O campo Numero deve ser preenchido').bail()
     .isInt().withMessage('O campo Numero aceita somente numeros'),
     check('Bairro')
     .notEmpty().withMessage('O campo Bairro deve ser preenchido')
-    .isAlpha().withMessage('Deve ser gigitado somente letras'),
+    .isAlpha().withMessage('Deve ser digitado somente letras'),
     check('Cidade')
     .notEmpty().withMessage('O campo Cidade deve ser preenchido')
-    .isAlpha().withMessage('Deve ser gigitado somente letras'),
+    .isAlpha().withMessage('Deve ser digitado somente letras'),
     check('Estado')
     .notEmpty().withMessage('O campo Estado deve ser preenchido'),
     check('CEP')
@@ -101,7 +101,7 @@ let validarCompra = [
     check('NomeCartao')
     .notEmpty().withMessage('O campo Nome no Cartao deve ser preenchido').bail()
     .isLength({min:8}).withMessage('O nome e muito curto!')
-    .isAlpha().withMessage('Deve ser gigitado somente letras'),
+    .isAlpha().withMessage('Deve ser digitado somente letras'),
     check('CodigoSeguranca')
     .notEmpty().withMessage('O campo Codigo de Seguranca deve ser preenchido').bail()
     .isInt().withMessage('O campo Codigo de Seguranca aceita somente numeros')

b768fb2b0bc87efb15191e1225c1044a19a87109
Peterson <petersoncabrini@hotmail.com>
2022-05-29 08:27:25 -0300

    ajuste na rota post

--- controllers/PainelController.js
+++ controllers/PainelController.js
@@ -15,6 +15,17 @@ const PainelController = {
         }
         console.log(req.body)
     },
+    painelLogado: function (req, res) {
+        if (req.session.idUsuario === undefined) {
+            res.redirect('/login');
+        } else {
+            res.render('painelLogado', {
+                nomeUsuario: req.session.nomeUsuario,
+                emailUsuario: req.session.emailUsuario,
+                idUsuario: req.session.idUsuario
+            });
+        }
+    }
 }
 
 module.exports = PainelController

d73bd2075de98f35cc927f4bee2e502e813f4be2
Peterson <petersoncabrini@hotmail.com>
2022-05-31 21:14:17 -0300

    removida a validacao .isAlpha() de alguns campos

--- middlewares/validarEditarPainel.js
+++ middlewares/validarEditarPainel.js
@@ -3,14 +3,11 @@ const {check} = require ('express-validator')
 let validarEditarPainel =[
     check('Nome')
     .notEmpty().withMessage('O campo Nome deve ser preenchido').bail()
-    .isLength({min:5}).withMessage('O Nome deve ter pelo menos 3 caracteres')
-    .isAlpha().withMessage('Deve ser digitado somente letras'),
+    .isLength({min:5}).withMessage('O Nome deve ter pelo menos 3 caracteres'),
     check('Sobrenome')
-    .notEmpty().withMessage('O campo Sobrenome deve ser preenchido')
-    .isAlpha().withMessage('Deve ser digitado somente letras'),
+    .notEmpty().withMessage('O campo Sobrenome deve ser preenchido'),
     check('Usuario')
-    .notEmpty().withMessage('O campo Usuario deve ser preenchido')
-    .isAlpha().withMessage('Deve ser digitado somente letras'),
+    .notEmpty().withMessage('O campo Usuario deve ser preenchido'),
     check('Email')
     .notEmpty().withMessage('O campo Email deve ser preenchido').bail()
     .isEmail().withMessage('Digite um email valido'),
@@ -19,20 +16,16 @@ let validarEditarPainel =[
     .isInt().withMessage('O campo CPF aceita somente numeros')
     .isLength({min:11, max:11}).withMessage('CPF Invalido'),
     check('Endereco')
-    .notEmpty().withMessage('O campo Endereco deve ser preenchido').bail()
-    .isAlpha().withMessage('Deve ser digitado somente letras'),
+    .notEmpty().withMessage('O campo Endereco deve ser preenchido').bail(),
     check('Numero')
     .notEmpty().withMessage('O campo Numero deve ser preenchido').bail()
     .isInt().withMessage('O campo Numero aceita somente numeros'),
     check('Bairro')
-    .notEmpty().withMessage('O campo Bairro deve ser preenchido')
-    .isAlpha().withMessage('Deve ser digitado somente letras'),
+    .notEmpty().withMessage('O campo Bairro deve ser preenchido'),
     check('Cidade')
-    .notEmpty().withMessage('O campo Cidade deve ser preenchido')
-    .isAlpha().withMessage('Deve ser digitado somente letras'),
+    .notEmpty().withMessage('O campo Cidade deve ser preenchido'),
     check('Estado')
-    .notEmpty().withMessage('O campo Estado deve ser preenchido')
-    .isAlpha().withMessage('Deve ser digitado somente letras'),
+    .notEmpty().withMessage('O campo Estado deve ser preenchido'),
     check('CEP')
     .notEmpty().withMessage('O campo CEP deve ser preenchido').bail()
     .isPostalCode('BR').withMessage('CEP Invalido')

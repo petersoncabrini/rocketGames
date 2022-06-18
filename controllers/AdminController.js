36ee85568d9d2d75cf773e0de69857c5ce6a3fbd
Peterson <petersoncabrini@hotmail.com>
2022-06-18 11:19:53 -0300

    criacao da model e do filtro findAll() comentadas

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -3,7 +3,11 @@ const db = require('../models');
 const AdminController = {
     index: (req, res) => {
 
-      db.Admin.findAll().then((data) => {
+    // findAll() para encontrar todos os dados registrados na tabela
+    // A função findAll() retorna uma promise portanto, a usamos para pegar o resultado da pesquisa.
+      db.Admin.findAll()
+    // O resultado será atribuído no parâmetro desta função, aqui o chamamos de data.
+      .then((data) => {
           res.send(JSON.stringify(data));
       }).catch((err) => {
           res.send(err);
--- models/Admin.js
+++ models/Admin.js
@@ -1,4 +1,8 @@
 module.exports = (sequelize, DataTypes) => {
+    // O método define() nos permite definir atribuições entre um modelo e uma tabela
+    // O primeiro é um alias que identifica o model, 
+    // o segundo é um objeto com a configuração das colunas do banco de dados,
+    // e o terceiro é outro objeto com configurações adicionais
     const Admin = sequelize.define('Admin',{
         login: DataTypes.STRING,
         nome: DataTypes.STRING,
@@ -8,6 +12,6 @@ module.exports = (sequelize, DataTypes) => {
         timestamps: false
     })
 
-
+    // O que define() retorna será armazenado em uma variável com o nome do model, para posteriormente ser retornado pela função que criamos.
     return Admin;
 }

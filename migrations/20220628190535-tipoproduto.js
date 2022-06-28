9bcd7a9a94a79b8a4391c65b1a52acd0b469c25c
Peterson <petersoncabrini@hotmail.com>
2022-06-28 20:10:39 -0300

    adicionada a tabela tipoproduto

--- migrations/20220628190535-tipoproduto.js
+++ migrations/20220628190535-tipoproduto.js
@@ -1,21 +1,18 @@
 'use strict';
 
+const { query } = require("express-validator");
+const { DATE } = require("sequelize");
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+
+    await queryInterface.createTable('tipoproduto', {
+    id: { type: Sequelize.INTEGER, UNSIGNED, autoIncrement:true, primaryKey:true, allowNull:false },
+    tipoproduto: Sequelize.STRING(50)
+     })},
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    queryInterface.dropTable('tipoproduto');
+
   }
 };

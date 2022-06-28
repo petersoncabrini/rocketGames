f87fe4f84341813cd481b19de1924424e1e98818
Lucas Brito <100387176+KzBrito@users.noreply.github.com>
2022-06-28 18:25:03 -0300

    Update Produto.js

--- models/Produto.js
+++ models/Produto.js
@@ -7,8 +7,14 @@ module.exports = (sequelize, DataTypes) => {
         timestamps: false
     })
 
-    Produto.associate = (models) => {
+  Produto.associate = (models) => {
         Produto.belongsTo(models.TipoConsole || models.TipoProduto, {foreignKey: 'tipoconsole_id', foreignKey: 'tipoproduto_id'})
+        Produto.belongsToMany(models.pedidos, {
+            through:'pedidos_has_pedidos',
+            foreignKey:'produto_idproduto',
+            otherKey:'pedidos_idpedidos'
+
+        })
     }
 
     return Produto;

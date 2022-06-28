7c74dbed2e748cfe6b83066a5c155869c9bd4cf0
Lucas Brito <100387176+KzBrito@users.noreply.github.com>
2022-06-28 17:08:57 -0300

    migrations

---
+++ migrations/20220628190353-usuario.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const { query } = require("express-validator");
+const { DATE } = require("sequelize");
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+     await queryInterface.createTable('usuario', {
+     id: { Sequelize,INTEGER,UNSIGNED,autoIncrement: true, primaryKey: true },
+     nome: Sequelize.STRING(45),
+     email: Sequelize.STRING(50),
+     telefone: Sequelize.STRING(11),
+     data_de_nascimento: Sequelize.DATE,
+     senha: Sequelize.STRING(45)
+        
+     })},
+
+
+
+  async down (queryInterface, Sequelize) {
+    queryInterface.dropTable('usuario');
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};

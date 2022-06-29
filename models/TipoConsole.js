3d9d96066d3dea0d4a532b1f3d4b6702cb78c20f
Lucas Brito <100387176+KzBrito@users.noreply.github.com>
2022-06-29 20:16:17 -0300

    Update TipoConsole.js

--- models/TipoConsole.js
+++ models/TipoConsole.js
@@ -1,11 +1,13 @@
 module.exports = (sequelize, DataTypes) => {
-    const TipoConsole = sequelize.define('TipoConsole',{
+    const TipoConsole = sequelize.define('TipoConsole', {
         tipoconsole: DataTypes.STRING,
     }, {
         tableName: 'tipoconsole',
         timestamps: false
     })
-   
-  
+
+    TipoConsole.associate = (models) => {
+        TipoConsole.hasMany(models.Produto, { foreignKey: 'tipoconsole_id' })
+    }
     return TipoConsole;
 }

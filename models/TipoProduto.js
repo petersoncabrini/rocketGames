ad3796ac8dfa9d8d01e07b49d2ad5e2d9ca182d6
Lucas Brito <100387176+KzBrito@users.noreply.github.com>
2022-06-29 20:16:30 -0300

    Update TipoProduto.js

--- models/TipoProduto.js
+++ models/TipoProduto.js
@@ -1,11 +1,13 @@
 module.exports = (sequelize, DataTypes) => {
-    const TipoProduto = sequelize.define('TipoProduto',{
+    const TipoProduto = sequelize.define('TipoProduto', {
         tipoproduto: DataTypes.STRING,
     }, {
         tableName: 'tipoproduto',
         timestamps: false
     })
-    
 
+    TipoProduto.associate = (models) => {
+        TipoProduto.hasMany(models.Produto, { foreignKey: 'tipoproduto_id' })
+    }
     return TipoProduto;
 }

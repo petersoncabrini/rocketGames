35626b93ce036b7fe50cf7b2a94a35d8927a0d6a
Peterson <petersoncabrini@hotmail.com>
2022-06-30 16:38:21 -0300

    validacoes ajustadas e renomeadas

--- middlewares/validações/validarLogin.js
+++ middlewares/validações/validarLogin.js
@@ -1,16 +1,12 @@
 const {check} = require ('express-validator')
 
-let validarCriarConta = [
-    check('name')
-    .notEmpty().withMessage('O campo nome deve ser preenchido').bail()
-    .isLength({min:5}).withMessage('O nome deve ter pelo menos 3 caracteres')
-    .isAlpha().withMessage('Deve ser digitado somente letras'),
+let validarLogin = [
     check('email')
     .notEmpty().withMessage('O campo email deve ser preenchido').bail()
     .isEmail().withMessage('Digite um email valido'),
-    check('password')
+    check('senha')
     .notEmpty().withMessage('O campo senha deve ser preenchido').bail()
     .isLength({min:6}).withMessage('A senha deve ter pelo menos 6 caracteres')
 ]
 
-module.exports = validarCriarConta
+module.exports = validarLogin

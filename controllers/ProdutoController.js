a6fc965e30a6c074c1ff5e03a8cab31e03d0e052
Lucas Brito <100387176+KzBrito@users.noreply.github.com>
2022-06-30 19:14:00 -0300

    Update ProdutoController.js

--- controllers/ProdutoController.js
+++ controllers/ProdutoController.js
@@ -1,9 +1,9 @@
 const db = require('../models');
 
 const ProdutoController = {
-    index: (req, res) => {
+    listar: (req, res) => {
         db.Produto.findAll().then((produtos) => {
-            res.render("produtos", {
+            res.render("admin/produtos/index", {
                 listaProdutos: produtos
             });
         }).catch((err) => {
@@ -15,7 +15,52 @@ const ProdutoController = {
         const produto = await db.Produto.findByPk(idProduto);
 
         res.send(produto);
-    }
-};
+    },
+    cadastrar: (req, res) => {
+        res.render("admin/produtos/form", { formAction: "/produtos/cadastrar" });
+    },
+
+    acaoCadastrar: async (req, res) => {
+        const { nome, tipoconsole, tipoproduto, preco } = req.body;
+
+        await db.Produto.create({
+            nome: nome,
+            tipoconsole: tipoconsole,
+            tipoproduto: tipoproduto,
+            preco: preco
+        })
+
+        res.redirect("/produtos");
+    },
+
+  /*  editar: async (req, res) => {
+        const { idProduto } = req.params;
+
+        const produto = await db.Produto.findByPk(idProduto);
+
+        res.render("ProdutoEditar", {
+            formAction: "alterar" + idProduto,
+            produto: produto,
+            
+        })
+
+    },
+    acaoEditar: async(req, res) => {
+        const { nome, tipoconsole, tipoproduto, preco } = req.body;
+        const { idProduto } = req.params;
+
+        await db.Produto.update({
+            nome: nome,
+            tipoconsole: tipoconsole,
+            tipoproduto: tipoproduto,
+            preco: preco
+        }, { where: {
+            id: idProduto
+        }});
+
+        res.redirect("/produtos/editar");
+    },
+*/
+}
 
 module.exports = ProdutoController;
